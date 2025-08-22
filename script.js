61ed4cc9a8d9833fc064acf0385d0802f8395fda
gabedeko <gabe.deko@gmail.com>
2025-08-22 14:10:42 -0400

    modify mobile nav

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function (){ 
+document.addEventListener('DOMContentLoaded', function () {
 
   let siteInfo = document.querySelectorAll('.site-info');
 
@@ -6,36 +6,54 @@ document.addEventListener('DOMContentLoaded', function (){
 
   siteInfo.forEach((infos, index) => {
 
-      infos.addEventListener('mouseover', function () {
-        homeBody.classList.add("zoom-bg");
-      });
+    infos.addEventListener('mouseover', function () {
+      homeBody.classList.add("zoom-bg");
+    });
 
-      infos.addEventListener('mouseout', function () {
-        homeBody.classList.remove("zoom-bg");
-      });
+    infos.addEventListener('mouseout', function () {
+      homeBody.classList.remove("zoom-bg");
+    });
 
-      console.log("mouseover event added to site-info " + index);
+    console.log("mouseover event added to site-info " + index);
 
 
   });
 
   fetch("nav.html")
-  .then((response) => response.text())
-  .then((data) => {
-    const navElement = document.querySelector("nav");
-    navElement.innerHTML = data;
+    .then((response) => response.text())
+    .then((data) => {
+      const navElement = document.querySelector("nav");
+      navElement.innerHTML = data;
 
-    // Add event listeners to the nav links
-    let navIcon = document.querySelectorAll('.nav-icon');
 
-    navIcon.forEach(item => {
-      console.log('test');
-      item.addEventListener('click', () => {
-        console.log('Item clicked:');
+
+      // Add event listeners to the nav Icon to open the mobile nav
+      let navIcon = document.querySelectorAll('.nav-icon');
+
+      let mobileNav = document.querySelectorAll('.mobile-nav');
+
+      let navLinks = document.querySelectorAll('.nav-links');
+
+
+      navIcon.forEach(icon => {
+        console.log('test');
+        icon.addEventListener('click', () => {
+          console.log('Item clicked:');
+
+          icon.classList.toggle('open');
+
+          navLinks.forEach(link => {
+            link.classList.toggle('open');
+          });
+          mobileNav.forEach(nav => {
+            nav.classList.toggle('open');
+          });
+
+
+        });
       });
     });
-  });
 
-  
-  
+
+
 });
